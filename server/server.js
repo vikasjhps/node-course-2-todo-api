cd25330ebfb770fba0ef580063d366455f0e2a01
Vikas Kumar <vikas.jhps@gmail.com>
2019-02-19 15:18:44 +0530

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,8 +21,16 @@ app.post('/todos', (req, res) => {
   });
 });
 
-//app.get()
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
 
 app.listen(3000, () => {
   console.log('Started on PORT 3000');
 });
+
+module.exports ={app};
